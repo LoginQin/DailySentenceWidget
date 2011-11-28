22031cbc7546e7a272aba1dc895928da85ae435b
Qin Wei <qinwei081@gmail.com>
2011-11-29 00:08:37 +0800

    修改了添加微件后删除，不删除定时服务的bug，修改程序布局为wrap_content
    Signed-off-by: Qin Wei <qinwei081@gmail.com>

--- src/com/dailysentence/widget/UpdateService.java
+++ src/com/dailysentence/widget/UpdateService.java
@@ -7,7 +7,6 @@ import java.util.Date;
 
 import org.json.JSONObject;
 
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -33,29 +32,45 @@ public class UpdateService extends Service {
 		// TODO Auto-generated method stub
 		super.onStart(intent, startId);
 		Log.d("Service", "Service start");
-		
-	//requestUpdate(manager.getAppWidgetIds(new ComponentName(this, MedAppWidget.class)));
-	//	AppWidgetManager awm = AppWidgetManager.getInstance(this);
-	//	RemoteViews views = new RemoteViews(this.getPackageName(), R.layout.daily_appwidget_layout);
-		
-		DataSource currData = new DataSource();
-		SimpleDateFormat formatter = new SimpleDateFormat ("yyyyMMdd");
-		Date curDate = new Date(System.currentTimeMillis());//获取当前时间
-		StringBuilder date = new StringBuilder(formatter.format(curDate));
-		//currData.setDate("20111113");
-		JSONObject jsonObject = null;
-		try {
-			Log.d("widget_onUpdate", "URI:"+currData.getSentenceUri());
-			jsonObject = currData.getRemoteJSONdata(currData.getSentenceUri());
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			Log.d("widget_onUpdate", "error  json1");
+		AppWidgetManager awm = AppWidgetManager.getInstance(this);
+		int[] ids = awm.getAppWidgetIds(new ComponentName(this,
+				DailyWidget.class));
+
+		// requestUpdate(manager.getAppWidgetIds(new ComponentName(this,
+		// MedAppWidget.class)));
+		// AppWidgetManager awm = AppWidgetManager.getInstance(this);
+		// RemoteViews views = new RemoteViews(this.getPackageName(),
+		// R.layout.daily_appwidget_layout);
+		if (ids.length >= 1) { //判断widget id数目如果为0，关闭定时器
+			DataSource currData = new DataSource();
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+			Date curDate = new Date(System.currentTimeMillis());// 获取当前时间
+			StringBuilder date = new StringBuilder(formatter.format(curDate));
+			// currData.setDate("20111113");
+			JSONObject jsonObject = null;
+			try {
+				Log.d("widget_onUpdate", "URI:" + currData.getSentenceUri());
+				jsonObject = currData.getRemoteJSONdata(currData
+						.getSentenceUri());
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				Log.d("widget_onUpdate", "error  json1");
+			}
+
+			updateAppWidget(this, jsonObject, date.toString());
+		} else {
+			Intent service = new Intent(this, UpdateService.class);
+			PendingIntent sender = PendingIntent
+					.getService(this, 0, service, 0);
+			AlarmManager am = (AlarmManager) this
+					.getSystemService(Context.ALARM_SERVICE);
+			am.cancel(sender);
+			stopSelf();
+			Log.d("SERVICE", "SERVICE stop");
+
 		}
-		
-		updateAppWidget(this, jsonObject, date.toString());
-       
 
 	}
 
@@ -89,7 +104,7 @@ public class UpdateService extends Service {
 		boolean success = true;
 		String trans = null;
 		String sentencepoint = null;
-		SimpleDateFormat oldformatter = new SimpleDateFormat ("yyyyMMdd");
+		SimpleDateFormat oldformatter = new SimpleDateFormat("yyyyMMdd");
 		SimpleDateFormat newformatter = new SimpleDateFormat("yyyy年MM月dd日");
 		Date updatetime = null;
 		String str_date = null;
@@ -108,7 +123,6 @@ public class UpdateService extends Service {
 				sentencepoint = sentencepoint.replace("{br}", "\n");
 				updatetime = oldformatter.parse(date.toString());
 				str_date = newformatter.format(updatetime);
-				
 
 			} catch (Exception e) {
 				Log.d("widget_onUpdate", "error  json2");
@@ -121,7 +135,7 @@ public class UpdateService extends Service {
 			sentencepoint = "数据源自沪江网 BY ChineseTiger";
 			str_date = "\n http://weibo.com/chinesetiger";
 			btn_restart.findViewById(R.id.btn_restart);
-			
+
 			views.setViewVisibility(R.id.btn_restart, Button.VISIBLE);
 
 			Intent service = new Intent(context, UpdateService.class);
@@ -131,13 +145,12 @@ public class UpdateService extends Service {
 
 		}
 
-		
-
 		views.setTextViewText(R.id.sentence, "       " + sentence);
 		views.setTextViewText(R.id.trans, trans);
 		views.setTextViewText(R.id.sentencepoint, "句子要点：\n" + sentencepoint);
-		views.setTextViewText(R.id.date, (CharSequence) str_date);
-		if(success) views.setViewVisibility(R.id.btn_restart, Button.GONE);
+		views.setTextViewText(R.id.date, (CharSequence) "每日英语@" + str_date);
+		if (success)
+			views.setViewVisibility(R.id.btn_restart, Button.GONE);
 		awm.updateAppWidget(new ComponentName(context, DailyWidget.class),
 				views);
 		setNextStartAlarm(context);
